dd0c0788b196b4f13bdc845167522ca1488ea41a
Ihtasham <chowdhuryihtasham42@gmail.com>
2022-08-13 14:04:45 +0100

    implemneted chess pieces

--- classes/Board.py
+++ classes/Board.py
@@ -1,11 +1,24 @@
 import pygame
 import constants
+from classes.chess_pieces.ChessPiece import ChessPiece
 
 class Board():
     def __init__(self, game):
         self.game = game
+        self.chess_pieces = [
+            ChessPiece(game, 0, 0, constants.BLACK),
+            ChessPiece(game, 1, 0, constants.WHITE)
+        ]
 
     def draw(self):
+        self.draw_board()
+        self.draw_chess_pieces()
+
+    def draw_chess_pieces(self):
+        for chess_piece in self.chess_pieces:
+            chess_piece.draw()
+
+    def draw_board(self):
         for row in range(constants.BOARD_SIZE):
             for col in range(constants.BOARD_SIZE):
                 tile_color = constants.BLACK if (row + col) % 2 else constants.WHITE
--- classes/chess_pieces/ChessPiece.py
+++ classes/chess_pieces/ChessPiece.py
@@ -1,7 +1,38 @@
+import constants
+import pygame
+
 class ChessPiece():
-    def __init__(self):
+    def __init__(self, game, x, y, team):
         pattern = [
             [0, 1, 0],
             [2, 1, 2],
             [0, 3, 0]
         ]
+
+        self.game = game 
+        self.x = x
+        self.y = y
+        self.team = team
+
+        self.size = constants.TILE_SIZE / 2
+
+    def draw(self):
+        rect = (
+            self.x * constants.TILE_SIZE + self.size / 2, 
+            self.y * constants.TILE_SIZE + self.size / 2,
+            self.size,
+            self.size
+        ),
+
+        pygame.draw.rect(
+            self.game.screen, 
+            self.team,
+            rect
+        )
+
+        pygame.draw.rect(
+            self.game.screen, 
+            constants.GRAY,
+            rect,
+            4
+        )
